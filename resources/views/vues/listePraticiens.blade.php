f2462f5f9afb0cba965f36ea6a93383845f21ab8
Matheo <matheo.gaillac@lmdsio.fr>
2024-03-21 15:36:24 +0100

    Add menu déroulant spécialités

--- resources/views/vues/listePraticiens.blade.php
+++ resources/views/vues/listePraticiens.blade.php
@@ -2,15 +2,15 @@
 @section('content')
 
     <label for="praticien_id">Sélectionner un praticien</label>
-    <select name="praticien_id">
+    <select name="praticien_id" id="praticien_id">
         <option value="" selected>Choisir un praticien</option>
         @foreach($mesPraticiens as $praticien)
             <option value="{{ $praticien->id_praticien }}">{{ $praticien->nom_praticien }} {{ $praticien->prenom_praticien }}</option>
         @endforeach
     </select>
 <BR>
-    <label for="praticien_id">Sélectionner une spécialité</label>
-    <select name="praticien_id">
+    <label for="specialite_id">Sélectionner une spécialité</label>
+    <select name="specialite_id" id="specialite_id">
         <option value="" selected>Choisir une spécialité</option>
         @foreach($mesSpecialites as $spe)
             <option value="{{ $spe->id_specialite }}">{{ $spe->lib_specialite }}</option>
